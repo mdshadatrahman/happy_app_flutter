2238e143d3855186a1e4ffc50e52e21e223606fa
mdshadatrahman <shadat.rahman.464@gmail.com>
2022-04-05 17:19:21 +0600

    initial commit

---
+++ android/app/src/main/kotlin/com/example/codecell_happy_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codecell_happy_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/Colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final blackH1 = Color(0xff0D2433);
+  static final blackH2 = Color(0xff040F15);
+  static final blackH3 = Color(0xff313D3D);
+  static final test = Color.fromARGB(255, 19, 211, 211);
+}
